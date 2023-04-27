8e4418297b0d43c7193629ba8c265c0d4f082eeb
Surajit Pal <palsurajit06@gmail.cpm>
2023-04-28 02:26:49 +0530

    task complete

--- src/screens/FlexBox.jsx
+++ src/screens/FlexBox.jsx
@@ -3,15 +3,33 @@ import React from 'react'
 
 const FlexBox = () => {
     return (
-        <View style={styles.body}>
-            <View style={styles.view1}>
-                <Text style={styles.text}>1</Text>
+        <View style={styles.parent}>
+            <View style={styles.row1}>
+                <View style={styles.box1}>
+                    <Text style={styles.text}>1</Text>
+                </View>
+                <View style={styles.box2}>
+                    <Text style={styles.text}>2</Text>
+                </View>
+                <View style={styles.box3}>
+                    <Text style={styles.text}>3</Text>
+                </View>
             </View>
-            <View style={styles.view2}>
-                <Text style={styles.text}>2</Text>
+
+            <View style={styles.box4}>
+                <Text style={styles.text}>4</Text>
             </View>
-            <View style={styles.view3}>
-                <Text style={styles.text}>3</Text>
+            <View style={styles.box5}>
+                <Text style={styles.text}>5</Text>
+            </View>
+
+            <View style={styles.row3}>
+                <View style={styles.box6}>
+                    <Text style={styles.text}>6</Text>
+                </View>
+                <View style={styles.box7}>
+                    <Text style={styles.text}>7</Text>
+                </View>
             </View>
         </View>
     )
@@ -20,36 +38,60 @@ const FlexBox = () => {
 export default FlexBox
 
 const styles = StyleSheet.create({
-    body: {
+    parent: {
         flex: 1,
-        backgroundColor: "#d7e7d8",
-        alignItems: "center",
-        justifyContent: "center",
     },
     text: {
-        // color: "#000000",
         fontSize: 35,
-        // margin: 10,
     },
-    view1: {
-        width: 100,
-        height: 100,
+    row1: {
+        flex: 1,
+        flexDirection: "row"
+    },
+    row3: {
+        flex: 6,
+        flexDirection: "row"
+    },
+    box1: {
+        flex: 1,
         backgroundColor: "#d76067",
         alignItems: "center",
         justifyContent: "center"
     },
-    view2: {
-        width: 100,
-        height: 100,
+    box2: {
+        flex: 2,
         backgroundColor: "#a7c44c",
         alignItems: "center",
         justifyContent: "center"
     },
-    view3: {
-        width: 100,
-        height: 100,
+    box3: {
+        flex: 3,
         backgroundColor: "#3fb59e",
         alignItems: "center",
         justifyContent: "center"
     },
+    box4: {
+        flex: 1,
+        backgroundColor: "red",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    box5: {
+        flex: 1,
+        backgroundColor: "yellow",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    box6: {
+        flex: 1,
+        backgroundColor: "white",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    box7: {
+        flex: 1,
+        backgroundColor: "blue",
+        alignItems: "center",
+        justifyContent: "center",
+    }
 })
